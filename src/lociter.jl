d8a361bf2bf94027d0ea2104d2642294325a2e0f
S R Gokul Krishnan <srgk26ster@gmail.com>
2019-11-26 22:14:11 +0000

    Update lociter.jl

--- src/lociter.jl
+++ src/lociter.jl
@@ -1,9 +1,13 @@
-immutable LocationIterator{w,T}
+struct LocationIterator{w,T}
     range::UnitRange{Int}
     index::FMIndex{w,T}
 end
 
 Base.length(iter::LocationIterator) = length(iter.range)
-@inline Base.start(iter::LocationIterator) = 1
-@inline Base.done(iter::LocationIterator, i) = i > length(iter)
-@inline Base.next(iter::LocationIterator, i) = sa_value(iter.range[i], iter.index) + 1, i + 1
+@inline function Base.iterate(iter::LocationIterator, state=1)
+    if i >= length(iter.range)
+        return nothing
+    end
+
+    return (sa_value(iter.range[i], iter.index) + 1, state + 1)
+end
